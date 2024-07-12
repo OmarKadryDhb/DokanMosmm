c9a4e35ff57bc8eafd5fc5864e4ea494c5a6f2a9
OmarKadryDhb <omardahab38@gmail.com>
2024-07-12 21:15:04 +0300

    translate

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import  translations  from "./translations.js";
+
 var content = document.getElementsByTagName("body")[0];
 var navbar = document.getElementById("navbar");
 var darkmode = document.getElementById("darkmode");
@@ -6,6 +8,9 @@ var homeH1 = document.getElementById("home");
 var services = document.getElementById("services");
 var supported = document.getElementById("supported");
 var regNow = document.getElementById("reg-now");
+var footer = document.getElementById("footer");
+var languageSel = document.getElementById("language-selector");
+var search = document.getElementById("search");
 
 darkmode.addEventListener('click', function () {
     darkmode.classList.toggle('active');
@@ -16,4 +21,34 @@ darkmode.addEventListener('click', function () {
     services.classList.toggle('night');
     supported.classList.toggle('night');
     regNow.classList.toggle('night');
+    footer.classList.toggle('night');
 })
+
+const languageSelector = document.querySelector('select');
+languageSelector.addEventListener('change', (event) => {
+    setLanguage(event.target.value);
+    localStorage.setItem('lang', event.target.value);
+    
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    setLanguage(localStorage.getItem('lang'));
+})
+const setLanguage = (language) =>{
+    const elements = document.querySelectorAll('[data-i18n]');
+
+    elements.forEach(element => {
+        const transKey = element.getAttribute('data-i18n');
+        element.textContent = translations[language][transKey]
+ })
+   
+    // languageSel.classList.toggle('active-lang');
+
+    // if (languageSel.classList.contains('active-lang')) {
+    //     search.a("placeholder").value = "ضع رابط الملف هنا"
+    //     console.log("asa");
+    // }
+
+    document.dir = language === 'ar' ? 'rtl' : 'ltr';
+}
