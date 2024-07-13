4cc3610e52494903b575081ccc48e842d0d8b07b
OmarKadryDhb <omardahab38@gmail.com>
2024-07-13 08:51:14 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -11,6 +11,12 @@ var regNow = document.getElementById("reg-now");
 var footer = document.getElementById("footer");
 var languageSel = document.getElementById("language-selector");
 var search = document.getElementById("search");
+var topicr_p = document.getElementById("topicr_p");
+var topicr_h3 = document.getElementById("topicr_h3");
+var topicl_h3 = document.getElementById("topicl_h3");
+var topicl_p = document.getElementById("topicl_p");
+
+
 
 darkmode.addEventListener('click', function () {
     darkmode.classList.toggle('active');
@@ -29,6 +35,7 @@ languageSelector.addEventListener('change', (event) => {
     setLanguage(event.target.value);
     localStorage.setItem('lang', event.target.value);
     
+  
 })
 
 
@@ -41,14 +48,29 @@ const setLanguage = (language) =>{
     elements.forEach(element => {
         const transKey = element.getAttribute('data-i18n');
         element.textContent = translations[language][transKey]
- })
-   
+    })
+
+    if (language === 'ar') {
+        topicr_p.dir = 'rtl';
+        topicr_h3.dir = 'ltr';
+        topicl_h3.dir = 'rtl';
+        topicl_p.dir = 'rtl';
+    }
+    else if(language === 'en'){
+        topicr_p.dir = 'ltr';
+        topicr_h3.dir = 'rtl';
+        topicl_h3.dir = 'ltr';
+        topicl_p.dir = 'ltr';
+    }
+
     // languageSel.classList.toggle('active-lang');
 
     // if (languageSel.classList.contains('active-lang')) {
     //     search.a("placeholder").value = "ضع رابط الملف هنا"
     //     console.log("asa");
     // }
+
+
 
     document.dir = language === 'ar' ? 'rtl' : 'ltr';
 }
